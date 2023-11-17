7283cace11e2c913c96c32e23eac50d2318dc05f
MarcPL <marclert@gmail.com>
2023-11-17 12:18:36 +0700

    adding css and more content

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog_index, name="blog_index"),
+    path("", views.blog_home, name="blog_home"),
+    path("post", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
+    path("project", views.project_index, name="project_index")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from .models import Post, Comment
+from .models import Post, Comment, Category
 from .forms import CommentForm
 from django.http import HttpResponseRedirect
+
 # Create your views here.
+
+def blog_home(request):
+    return render(request, 'blog/home.html')
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
+    categories = Category.objects.all()    
     context = {
         'posts' : posts,
+        'categories' : categories,
     }
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/blog.html', context)
 
 def blog_category(request, category):
     post = Post.objects.filter(categories__name__contains=category).order_by('-created_on')
@@ -42,3 +48,6 @@ def blog_detail(request, pk):
         'form' : form,
     }
     return render(request, 'blog/details.html', context)
+
+def project_index(request):
+    return render(request, 'blog/project.html')
